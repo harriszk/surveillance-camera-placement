0a6786053594a6314a42a5c379fafbdb880868fc
harriszk <zacharykeatonharris@gmail.com>
2025-03-11 21:02:29 -0400

    fix: refactored point placer tool

--- src/features/scene/PointPlacementTool.tsx
+++ src/features/scene/PointPlacementTool.tsx
@@ -1,47 +1,48 @@
 import { Sphere } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useState } from "react";
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import * as THREE from "three";
 import { addVertex } from "./sceneSlice";
 
 const PointPlacementTool: React.FC = () => {
-    const [hoveredPoint, setHoveredPoint] = useState<
-        [number, number, number] | null
-    >(null);
-    const { camera, pointer } = useThree();
     const dispatch = useDispatch();
+    const { camera, pointer } = useThree();
+
+    const sphereRef = useRef<THREE.Mesh>(null);
+    const hoveredPointRef = useRef<[number, number, number] | null>(null);
+    const raycaster = useRef(new THREE.Raycaster());
+    const plane = useRef(new THREE.Plane(new THREE.Vector3(0, 1, 0), 0));
+    const intersection = new THREE.Vector3();
 
     useFrame(() => {
-        const raycaster = new THREE.Raycaster();
-        raycaster.setFromCamera(pointer, camera);
-        const plane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0); // X-Z Plane at Y=0
-        const intersection = new THREE.Vector3();
-
-        if (raycaster.ray.intersectPlane(plane, intersection)) {
-            setHoveredPoint(intersection.toArray());
+        raycaster.current.setFromCamera(pointer, camera);
+        if (raycaster.current.ray.intersectPlane(plane.current, intersection)) {
+            hoveredPointRef.current = intersection.toArray();
+            if (sphereRef.current) {
+                sphereRef.current.position.set(...hoveredPointRef.current);
+            }
         }
     });
 
+    const handleClick = () => {
+        if (!hoveredPointRef.current) {
+            return;
+        }
+
+        dispatch(
+            addVertex({
+                key: "",
+                position: hoveredPointRef.current,
+                color: "#A1C3FF",
+            }),
+        );
+    };
+
     return (
-        hoveredPoint && (
-            <Sphere
-                key={"hovered-point"}
-                args={[0.035]}
-                position={hoveredPoint}
-                onClick={() => {
-                    dispatch(
-                        addVertex({
-                            key: "",
-                            position: hoveredPoint,
-                            color: "#A1C3FF",
-                        }),
-                    );
-                }}
-            >
-                <meshBasicMaterial color="blue" />
-            </Sphere>
-        )
+        <Sphere ref={sphereRef} args={[0.035]} onClick={handleClick}>
+            <meshBasicMaterial color="blue" />
+        </Sphere>
     );
 };
 
